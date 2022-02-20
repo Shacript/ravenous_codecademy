35b3102b615570e833251cbefab56ffa991f9123
Aphisit Likitwattanapaisarn <aphisit.heung@gmail.com>
2022-02-20 22:22:24 +0700

    add method ( handleTermChange , handleLocationChange )

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -14,6 +14,8 @@ class SearchBar extends React.Component {
       "Highest Rated": "rating",
       "Most Reviewed": "review_count",
     };
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
   }
   getSortByClass(sortByOption) {
     return this.state.sortBy === sortByOption ? "active" : "";
@@ -21,13 +23,19 @@ class SearchBar extends React.Component {
   handleSortByChange(sortByOption) {
     this.setState({ sortBy: sortByOption });
   }
+  handleTermChange(e) {
+    this.setState({ term: e.target.value });
+  }
+  handleLocationChange(e) {
+    this.setState({ location: e.target.value });
+  }
   renderSortByOptions() {
     return Object.keys(this.sortByOptions).map((sortByOption) => {
       let sortByOptionValue = this.sortByOptions[sortByOption];
       return (
         <li
           className={this.getSortByClass(sortByOptionValue)}
-          onClick={this.handleSortByChange.bind(this,sortByOptionValue)}
+          onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
           key={sortByOptionValue}
         >
           {sortByOption}
@@ -42,8 +50,8 @@ class SearchBar extends React.Component {
           <ul>{this.renderSortByOptions()}</ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+          <input placeholder="Where?" onChange={this.handleTermChange} />
         </div>
         <div className="SearchBar-submit">
           <a href="shacript.github.io" target="_blank">
